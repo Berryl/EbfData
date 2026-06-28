4cfc937df2417c397444bf145ef8463f59ed01e4
EBF <fingerhut22@gmail.com>
2026-06-28 11:07:30 -0400

    Add `SymbolTranslator` utility with `to_cagr_values` method and coverage tests
    - Implemented `SymbolTranslator` to handle symbol translation logic.
    - Added `to_cagr_values` method for parsing symbols and identifying tranches.
    - Included unit tests to ensure consistent functionality.

---
+++ src/ebf_data/orchestrators/symbol_translator.py
@@ -0,0 +1,39 @@
+from ebf_core.guards import guards as g
+
+from ebf_data.excel.cagr.cagr_table import CagrTable
+
+
+class SymbolTranslator:
+    def __init__(self, cagr: CagrTable) -> None:
+        g.ensure_not_none(cagr, "cagr")
+
+        self._cagr = cagr
+
+    def to_cagr_values(self, snapshot_symbol: str) -> tuple[str, int]:
+        """
+
+        :param snapshot_symbol:
+        :return: a tuple of (symbol, id, has_tranches)
+
+        For example, 'MARA_14.2' implies there is a 'MARA_14.1' and that there were tranches,
+        different fill times, prices, expiration dates, etc.
+        """
+        g.ensure_str_is_valued(snapshot_symbol, "snapshot_symbol")
+
+        symbol = snapshot_symbol
+        id_val = -1
+        has_tranches = False
+        if "_" in snapshot_symbol:
+            result = snapshot_symbol.split("_")
+            symbol = result[0]
+            result = result[1]
+            if "." in result:
+                result = result.split(".")
+                id_val = int(result[0])
+                has_tranches = True
+            else:
+                id_val = int(result)
+        else:
+
+            id_val = self._cagr.max_id_for_symbol(symbol)
+        return symbol, id_val, has_tranches
---
+++ tests/orchestrators/test_symbol_translator.py
@@ -0,0 +1,30 @@
+import pytest
+
+from ebf_data.excel.cagr.cagr_table import CagrTable
+from ebf_data.orchestrators.symbol_translator import SymbolTranslator
+
+
+class TestSymbolTranslator:
+    @pytest.fixture
+    def sut(self) -> SymbolTranslator:
+        return SymbolTranslator(CagrTable())
+
+    class TestToCagrValues:
+
+        class TestWhenSymbolHasNoSuffix:
+            def test_trade_id_is_max_id_found(self, sut: SymbolTranslator) -> None:
+                result = sut.to_cagr_values("YUM")
+                symbol = result[0]
+                id_val = result[1]
+                has_tranches = result[2]
+
+                assert symbol == "YUM" and id_val == 1 and not has_tranches
+
+        class TestWhenSymbolHasSuffixWithoutTranche:
+            def test_trade_id_is_max_id_found(self, sut: SymbolTranslator) -> None:
+                result = sut.to_cagr_values("FCX_20")
+                symbol = result[0]
+                id_val = result[1]
+                has_tranches = result[2]
+
+                assert symbol == "FCX" and id_val == 20 and not has_tranches
